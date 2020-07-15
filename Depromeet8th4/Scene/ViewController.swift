b7866e94bbb2830086c75b4d590993c9d063ff1f
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-07-15 23:52:25 +0900

    init

---
+++ Depromeet8th4/Scene/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Depromeet8th4
+//
+//  Created by Soso on 2020/07/15.
+//  Copyright © 2020 Depromeet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
