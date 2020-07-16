8f97b5ac692f47df8c7eb8f7e523f220feca2c35
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-07-16 16:18:59 +0900

    add: BaseService, ServiceProvider, NetworkService

---
+++ Depromeet8th4/Service/NetworkService.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkService.swift
+//  Depromeet8th4
+//
+//  Created by Soso on 2020/07/16.
+//  Copyright © 2020 Depromeet. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkServiceType {
+
+}
+
+class NetworkService: BaseService, NetworkServiceType {
+
+}
