72940fdd7d3032d11457478332f8fe8d2f5c0346
yeojaeng <duwjdtn1@gmail.com>
2020-07-27 23:51:12 +0900

    PR Test

---
+++ Depromeet8th4/Scene/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  Depromeet8th4
+//
+//  Created by Yeojaeng on 2020/07/27.
+//  Copyright © 2020 Depromeet. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class DetailViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+    
+}
