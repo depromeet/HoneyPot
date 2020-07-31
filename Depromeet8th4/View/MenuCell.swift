568d35ddaa1bc1e3ea458176bb34a22defe16a61
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-07-31 20:36:10 +0900

    add: MyPageViewController tableView, menuCell

---
+++ Depromeet8th4/View/MenuCell.swift
@@ -0,0 +1,39 @@
+//
+//  MenuCell.swift
+//  Depromeet8th4
+//
+//  Created by Soso on 2020/07/31.
+//  Copyright © 2020 Depromeet. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class MenuCell: BaseTableViewCell {
+    let imageViewIcon = UIImageView().then {
+        $0.image = nil
+    }
+    let labelTitle = UILabel().then {
+        $0.textColor = .black
+        $0.font = .systemFont(ofSize: 16)
+    }
+
+    override func initialize() {
+        setupViews()
+    }
+
+    private func setupViews() {
+        selectionStyle = .none
+        addSubview(imageViewIcon)
+        imageViewIcon.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(18)
+            $0.centerY.equalToSuperview()
+        }
+        addSubview(labelTitle)
+        labelTitle.snp.makeConstraints {
+            $0.leading.equalTo(imageViewIcon.snp.trailing).offset(10)
+            $0.centerY.equalToSuperview()
+        }
+    }
+}
