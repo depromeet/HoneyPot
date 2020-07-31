49bcc9fb127f095a1c8c45a63da51928084519e4
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-07-31 19:29:40 +0900

    add: MyPageViewController info

---
+++ Depromeet8th4/View/VerticalButton.swift
@@ -0,0 +1,25 @@
+//
+//  VerticalButton.swift
+//  Depromeet8th4
+//
+//  Created by Soso on 2020/07/31.
+//  Copyright © 2020 Depromeet. All rights reserved.
+//
+
+import UIKit
+
+class VerticalButton: UIButton {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        let width = bounds.width
+        let imageViewHeight = bounds.height / 5 * 3
+        let labelHeight = bounds.height / 5 * 2
+        let imageViewFrame = CGRect(x: 0, y: 0, width: width, height: imageViewHeight)
+        let labelFrame = CGRect(x: 0, y: imageViewHeight, width: width, height: labelHeight)
+
+        imageView?.frame = imageViewFrame
+        titleLabel?.frame = labelFrame
+
+    }
+}
