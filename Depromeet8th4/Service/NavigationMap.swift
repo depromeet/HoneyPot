08a751c16786a2e810bc9031f459e8ca585bf5cf
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-08-05 21:30:14 +0900

    add: NavigationMap

---
+++ Depromeet8th4/Service/NavigationMap.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationMap.swift
+//  Depromeet8th4
+//
+//  Created by Soso on 12/04/2020.
+//  Copyright © 2020 Soso. All rights reserved.
+//
+
+import UIKit
+import URLNavigator
+
+struct NavigationMap {
+    static func initialize(navigator: NavigatorType, viewController: UIViewController, provider: ServiceProviderType) {
+        guard let navigationController = viewController as? UINavigationController else { return }
+        navigator.register("honeypot://item/<id>") { (url, values, context) in
+            print(url, values, context)
+            return nil
+        }
+    }
+}
