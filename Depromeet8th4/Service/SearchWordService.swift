363b7fbce01cd683dc5225538d96b0682c833770
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-08-08 19:08:09 +0900

    add: SearchWordService

---
+++ Depromeet8th4/Service/SearchWordService.swift
@@ -0,0 +1,69 @@
+//
+//  SearchWordService.swift
+//  Depromeet8th4
+//
+//  Created by Soso on 2020/08/08.
+//  Copyright © 2020 Depromeet. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+extension UserDefaultsKey {
+    static var searchWords: Key<[String: Double]> {
+        return "searchWords"
+    }
+}
+
+enum WordEvent {
+    case add(String)
+    case remove(String)
+}
+
+protocol SearchWordServiceType {
+    func getWords() -> Observable<[String: Double]>
+    func addWord(word: String) -> Observable<[String: Double]>
+    func removeWord(word: String) -> Observable<[String: Double]>
+}
+
+final class SearchWordService: BaseService, SearchWordServiceType {
+    @discardableResult
+    private func saveWords(_ words: [String: Double]) -> Observable<Void> {
+        self.provider.userDefaultsService.set(value: words, forKey: .searchWords)
+        return .just(Void())
+    }
+
+    func getWords() -> Observable<[String: Double]> {
+        if let words = provider.userDefaultsService.value(forKey: .searchWords) {
+            return .just(words)
+        } else {
+            return .just([:])
+        }
+    }
+
+    func addWord(word: String) -> Observable<[String: Double]> {
+        return getWords()
+            .flatMap({ [weak self] words -> Observable<Void> in
+                guard let self = self else { return .empty() }
+                var words = words
+                words[word] = Date().timeIntervalSince1970
+                return self.saveWords(words)
+            })
+            .flatMap({ [weak self] in
+                return self?.getWords() ?? .empty()
+            })
+    }
+
+    func removeWord(word: String) -> Observable<[String: Double]> {
+        return getWords()
+            .flatMap({ [weak self] words -> Observable<Void> in
+                guard let self = self else { return .empty() }
+                var words = words
+                words[word] = nil
+                return self.saveWords(words)
+            })
+            .flatMap({ [weak self] in
+                return self?.getWords() ?? .empty()
+            })
+    }
+}
