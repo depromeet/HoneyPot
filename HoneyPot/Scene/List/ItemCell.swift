b834389efbea8ecddb7dad7b84b7be643ab4883a
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-08-18 22:23:00 +0900

    edit: ItemCell layout

--- HoneyPot/Scene/List/ItemCell.swift
+++ HoneyPot/Scene/List/ItemCell.swift
@@ -16,20 +16,43 @@ class ItemCell: BaseTableViewCell {
         static let mainTitle = 0x323232.color
         static let categoryTitle = 0x464646.color
         static let priceFocusedTitle = 0xEB5757.color
+        static let countTitle = 0x8C8C8C.color
     }
     private enum Font {
-        static let likeCommentTitle = UIFont(name: "AppleSDGothicNeo-Bold", size: 12)
-        static let mainTitle = UIFont.systemFont(ofSize: 20, weight: .bold)
-        static let categoryTitle = UIFont.systemFont(ofSize: 14)
-        static let focusedTitle = UIFont(name: "AppleSDGothicNeo-Bold", size: 12)!
-        static let normalTitle = UIFont(name: "AppleSDGothicNeo-Regular", size: 12)!
-        static let priceBigTitle = UIFont(name: "AppleSDGothicNeo-Bold", size: 24)!
-        static let priceSmallTitle = UIFont(name: "AppleSDGothicNeo-Regular", size: 14)!
+        static let godoB20 = UIFont(name: "GodoB", size: 20)!
+        static let sdR16 = UIFont(name: "AppleSDGothicNeo-Regular", size: 16)!
+        static let sdB24 = UIFont(name: "AppleSDGothicNeo-Bold", size: 24)!
+        static let sdB14 = UIFont(name: "AppleSDGothicNeo-Bold", size: 14)!
+        static let sdB12 = UIFont(name: "AppleSDGothicNeo-Bold", size: 12)!
+        static let sdR14 = UIFont(name: "AppleSDGothicNeo-Regular", size: 14)!
+        static let sdR12 = UIFont(name: "AppleSDGothicNeo-Regular", size: 12)!
     }
     private enum Metric {
         static let leadingOffset: CGFloat = 18
         static let trailingOffset: CGFloat = 18
     }
+    private enum Style {
+        static let paragraph = NSMutableParagraphStyle().then {
+            $0.lineSpacing = 4
+        }
+        static let itemText: [NSAttributedString.Key: Any] = [
+            .kern: -0.6,
+            .font: Font.godoB20,
+            .foregroundColor: Color.mainTitle,
+            .paragraphStyle: paragraph
+        ]
+        static let countNormalText: [NSAttributedString.Key: Any] = [
+            .kern: -0.3,
+            .font: Font.sdR14,
+            .foregroundColor: Color.countTitle
+        ]
+        static let countFocusText: [NSAttributedString.Key: Any] = [
+            .kern: -0.3,
+            .font: Font.sdB14,
+            .foregroundColor: Color.countTitle
+        ]
+    }
+
     let viewContainer = UIView().then {
         $0.backgroundColor = .clear
     }
@@ -45,11 +68,11 @@ class ItemCell: BaseTableViewCell {
         $0.setImage(#imageLiteral(resourceName: "icon_heart_w16h16"), for: .normal)
         $0.setTitle("302", for: .normal)
         $0.setTitleColor(Color.likeCommentTitle, for: .normal)
-        $0.titleLabel?.font = Font.likeCommentTitle
+        $0.titleLabel?.font = Font.sdB12
         $0.backgroundColor = .white
         $0.layer.cornerRadius = 5
         $0.layer.masksToBounds = true
-        $0.contentEdgeInsets = .init(top: 5, left: 6, bottom: 5, right: 9)
+        $0.contentEdgeInsets = .init(top: 5, left: 5, bottom: 5, right: 9)
         $0.titleEdgeInsets = .init(top: 0, left: 3, bottom: 0, right: -3)
     }
     let buttonComment = UIButton().then {
@@ -57,79 +80,68 @@ class ItemCell: BaseTableViewCell {
         $0.setImage(#imageLiteral(resourceName: "icon_bubble_w16h16"), for: .normal)
         $0.setTitle("13", for: .normal)
         $0.setTitleColor(Color.likeCommentTitle, for: .normal)
-        $0.titleLabel?.font = Font.likeCommentTitle
+        $0.titleLabel?.font = Font.sdB12
         $0.backgroundColor = .white
         $0.layer.cornerRadius = 5
         $0.layer.masksToBounds = true
-        $0.contentEdgeInsets = .init(top: 5, left: 6, bottom: 5, right: 9)
+        $0.contentEdgeInsets = .init(top: 5, left: 5, bottom: 5, right: 9)
         $0.titleEdgeInsets = .init(top: 0, left: 3, bottom: 0, right: -3)
     }
 
     let labelTitle = UILabel().then {
-        $0.textColor = Color.mainTitle
-        $0.font = UIFont(name: "GodoB", size: 20)
-        $0.text = "테이블팬 C820 (3 Colors)"
+        let attributedString = NSAttributedString(string: "테이블팬 C820 (3 Colors)", attributes: Style.itemText)
+        $0.attributedText = attributedString
         $0.numberOfLines = 2
     }
     let labelCategory = UILabel().then {
         $0.textColor = Color.categoryTitle
-        $0.font = Font.categoryTitle
-        $0.text = "가전 | 디프만컴파니"
+        $0.font = Font.sdR16
+        $0.text = "가전  I  플러스마이너스제로"
     }
     var constraintProgressTrailing: NSLayoutConstraint!
 
-    let labelCount = UILabel().then {
-        let first = NSAttributedString(string: "426명 ", attributes: [
-                .font: Font.focusedTitle,
-                .foregroundColor: Color.likeCommentTitle
-            ])
-        let second = NSAttributedString(string: "참여중", attributes: [
-                .font: Font.normalTitle,
-                .foregroundColor: Color.likeCommentTitle
-            ])
+    let buttonCount = UIButton().then {
+        let first = NSAttributedString(string: "426명", attributes: Style.countFocusText)
+        let second = NSAttributedString(string: " 참여중", attributes: Style.countNormalText)
         let attributedText = NSMutableAttributedString()
         attributedText.append(first)
         attributedText.append(second)
-        $0.attributedText = attributedText
+        $0.setAttributedTitle(attributedText, for: .normal)
+        $0.setImage(#imageLiteral(resourceName: "icon_people_w18h18"), for: .normal)
+        $0.titleEdgeInsets = .init(top: 0, left: 5, bottom: 0, right: -5)
+        $0.contentEdgeInsets = .init(top: 0, left: 0, bottom: 0, right: 5)
+        $0.isUserInteractionEnabled = false
     }
-    let labelTime = UILabel().then {
-        let first = NSAttributedString(string: "4일 21시간 ", attributes: [
-                .font: Font.focusedTitle,
-                .foregroundColor: Color.likeCommentTitle
-            ])
-        let second = NSAttributedString(string: "남음", attributes: [
-                .font: Font.normalTitle,
-                .foregroundColor: Color.likeCommentTitle
-            ])
+    let buttonTime = UIButton().then {
+        let first = NSAttributedString(string: "4일 21시간", attributes: Style.countFocusText)
+        let second = NSAttributedString(string: " 남음", attributes: Style.countNormalText)
         let attributedText = NSMutableAttributedString()
         attributedText.append(first)
         attributedText.append(second)
-        $0.attributedText = attributedText
+        $0.setAttributedTitle(attributedText, for: .normal)
+        $0.setImage(#imageLiteral(resourceName: "icon_clock_w18h18"), for: .normal)
+        $0.titleEdgeInsets = .init(top: 0, left: 5, bottom: 0, right: -5)
+        $0.contentEdgeInsets = .init(top: 0, left: 0, bottom: 0, right: 5)
+        $0.isUserInteractionEnabled = false
     }
-    let labelPrice = UILabel().then {
-        let first = NSAttributedString(string: "35% ", attributes: [
-                .font: Font.priceBigTitle,
-                .foregroundColor: Color.priceFocusedTitle
-            ])
-        let second = NSAttributedString(string: "32,500", attributes: [
-                .font: Font.priceBigTitle,
-                .foregroundColor: Color.mainTitle
-            ])
-        let attributedText = NSMutableAttributedString()
-        attributedText.append(first)
-        attributedText.append(second)
-        $0.attributedText = attributedText
+    let labelPricePercent = UILabel().then {
+        $0.font = Font.sdB24
+        $0.textColor = Color.priceFocusedTitle
+        $0.text = "35%"
+    }
+    let labelPriceDiscount = UILabel().then {
+        $0.font = Font.sdB24
+        $0.textColor = Color.mainTitle
+        $0.text = "32,500"
     }
     let labelCurrency = UILabel().then {
-        let attributedText = NSAttributedString(string: " 원", attributes: [
-                .font: Font.priceSmallTitle,
-                .foregroundColor: Color.mainTitle
-            ])
-        $0.attributedText = attributedText
+        $0.font = Font.sdR14
+        $0.textColor = Color.mainTitle
+        $0.text = "원"
     }
     let labelOverview = UILabel().then {
         $0.textColor = Color.likeCommentTitle
-        $0.font = Font.normalTitle
+        $0.font = Font.sdR12
         $0.text = "40% 할인까지 24명!"
     }
 
@@ -175,7 +187,7 @@ class ItemCell: BaseTableViewCell {
         }
         viewContainer.addSubview(labelCategory)
         labelCategory.snp.makeConstraints {
-            $0.top.equalTo(labelTitle.snp.bottom).offset(10)
+            $0.top.equalTo(labelTitle.snp.bottom).offset(7)
             $0.leading.trailing.equalToSuperview().inset(Metric.leadingOffset)
         }
     }
@@ -185,7 +197,7 @@ class ItemCell: BaseTableViewCell {
         }
         viewContainer.addSubview(viewProgressBackground)
         viewProgressBackground.snp.makeConstraints {
-            $0.top.equalTo(labelCategory.snp.bottom).offset(13)
+            $0.top.equalTo(labelCategory.snp.bottom).offset(10)
             $0.leading.trailing.equalToSuperview().inset(Metric.leadingOffset)
             $0.height.equalTo(4)
         }
@@ -200,44 +212,37 @@ class ItemCell: BaseTableViewCell {
         constraintProgressTrailing.isActive = true
     }
     private func setupInfo() {
-        let imageViewCount = UIImageView(image: #imageLiteral(resourceName: "icon_people_w18h18"))
-        viewContainer.addSubview(imageViewCount)
-        imageViewCount.snp.makeConstraints {
-            $0.top.equalTo(labelCategory.snp.bottom).offset(27)
+        viewContainer.addSubview(buttonCount)
+        buttonCount.snp.makeConstraints {
+            $0.top.equalTo(labelCategory.snp.bottom).offset(25)
             $0.leading.equalToSuperview().inset(Metric.leadingOffset)
-            $0.width.height.equalTo(18)
+            $0.height.equalTo(18)
         }
-        viewContainer.addSubview(labelCount)
-        labelCount.snp.makeConstraints {
-            $0.leading.equalTo(imageViewCount.snp.trailing).offset(5)
-            $0.centerY.equalTo(imageViewCount).offset(1)
-        }
-        let imageViewTime = UIImageView(image: #imageLiteral(resourceName: "icon_clock_w18h18"))
-        viewContainer.addSubview(imageViewTime)
-        imageViewTime.snp.makeConstraints {
-            $0.top.equalTo(imageViewCount.snp.bottom).offset(4)
-            $0.leading.equalToSuperview().inset(Metric.leadingOffset)
-            $0.width.height.equalTo(18)
-            $0.bottom.equalToSuperview().inset(20)
-        }
-        viewContainer.addSubview(labelTime)
-        labelTime.snp.makeConstraints {
-            $0.leading.equalTo(imageViewTime.snp.trailing).offset(5)
-            $0.centerY.equalTo(imageViewTime).offset(1)
+        viewContainer.addSubview(buttonTime)
+        buttonTime.snp.makeConstraints {
+            $0.top.equalTo(buttonCount.snp.bottom).offset(4)
+            $0.leading.equalTo(buttonCount)
+            $0.height.equalTo(18)
+            $0.bottom.equalToSuperview().inset(18)
         }
         viewContainer.addSubview(labelCurrency)
         labelCurrency.snp.makeConstraints {
-            $0.top.equalTo(labelCategory.snp.bottom).offset(32)
+            $0.top.equalTo(labelCategory.snp.bottom).offset(29)
             $0.trailing.equalToSuperview().inset(Metric.trailingOffset)
         }
-        viewContainer.addSubview(labelPrice)
-        labelPrice.snp.makeConstraints {
-            $0.trailing.equalTo(labelCurrency.snp.leading)
+        viewContainer.addSubview(labelPriceDiscount)
+        labelPriceDiscount.snp.makeConstraints {
+            $0.trailing.equalTo(labelCurrency.snp.leading).offset(-3)
             $0.bottom.equalTo(labelCurrency).inset(-5)
+        }
+        viewContainer.addSubview(labelPricePercent)
+        labelPricePercent.snp.makeConstraints {
+            $0.trailing.equalTo(labelPriceDiscount.snp.leading).offset(-7)
+            $0.centerY.equalTo(labelPriceDiscount)
         }
         viewContainer.addSubview(labelOverview)
         labelOverview.snp.makeConstraints {
-            $0.top.equalTo(labelCurrency.snp.bottom).offset(7)
+            $0.top.equalTo(labelCurrency.snp.bottom).offset(5)
             $0.trailing.equalTo(labelCurrency)
         }
     }
