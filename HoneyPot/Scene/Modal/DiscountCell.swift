5acac520fc67d52a172d28dbcf27bec66cd77502
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-08-19 11:00:44 +0900

    edit: DiscountModalViewController layout and attributes

--- HoneyPot/Scene/Modal/DiscountCell.swift
+++ HoneyPot/Scene/Modal/DiscountCell.swift
@@ -13,11 +13,11 @@ class DiscountCell: BaseTableViewCell {
         $0.image = nil
     }
     let labelPercent = UILabel().then {
-        $0.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 14)
+        $0.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 16)
         $0.textColor = 0x323232.color
     }
     let labelCount = UILabel().then {
-        $0.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 12)
+        $0.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 14)
         $0.textColor = 0x8C8C8C.color
     }
 
@@ -25,18 +25,18 @@ class DiscountCell: BaseTableViewCell {
         selectionStyle = .none
         addSubview(imageViewStep)
         imageViewStep.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(16)
+            $0.leading.equalToSuperview().inset(20)
             $0.centerY.equalToSuperview()
         }
         addSubview(labelPercent)
         labelPercent.snp.makeConstraints {
-            $0.leading.equalTo(imageViewStep.snp.trailing).offset(8)
-            $0.centerY.equalTo(imageViewStep)
+            $0.leading.equalTo(imageViewStep.snp.trailing).offset(12)
+            $0.centerY.equalTo(imageViewStep).offset(1)
         }
         addSubview(labelCount)
         labelCount.snp.makeConstraints {
             $0.leading.equalTo(labelPercent.snp.trailing).offset(8)
-            $0.centerY.equalTo(imageViewStep)
+            $0.centerY.equalTo(labelPercent).offset(1)
         }
     }
 }
